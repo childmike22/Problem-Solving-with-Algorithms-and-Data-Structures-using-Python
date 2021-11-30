2c7df07bf8541bdfd6998aa307dd0789f920b15d
childmike22 <58493509+childmike22@users.noreply.github.com>
2021-11-29 17:09:39 -0800

    Update pop_speed.py

--- c2_Algorithm_Analysis/pop_speed.py
+++ c2_Algorithm_Analysis/pop_speed.py
@@ -8,3 +8,16 @@ x = list(range(2000000))
 print(f"POP START: {pop_zero.timeit(number=1000):.9f}")
 
 print(f"POP END: {pop_end.timeit(number=1000):.9f}")
+
+
+# Prove that pop(0) is O(n) while pop() is O(1) - see below
+
+pop_zero = Timer("x.pop(0)", "from __main__ import x")
+pop_end = Timer("x.pop()", "from __main__ import x")
+print("pop(0) pop()")
+for i in range(1000000,100000001,1000000):
+	x = list(range(i))
+	pt = pop_end.timeit(number=1000)
+	x = list(range(i))
+	pz = pop_zero.timeit(number=1000)
+	print("%15.5f, %15.5f" %(pz,pt))
