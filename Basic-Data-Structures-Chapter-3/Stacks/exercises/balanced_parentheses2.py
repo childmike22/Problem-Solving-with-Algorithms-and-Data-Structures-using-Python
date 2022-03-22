694f101b2e4f58670779f7cd603fce177efa5632
childmike22 <58493509+childmike22@users.noreply.github.com>
2022-03-21 18:39:48 -0700

    Create balanced_parentheses2.py

---
+++ Basic-Data-Structures-Chapter-3/Stacks/exercises/balanced_parentheses2.py
@@ -0,0 +1,31 @@
+symbol_dict = {
+	'(': ')', 
+	'{':'}', 
+	'[': ']'
+}
+
+
+
+def balanced_parentheses(string):
+    opening_list = []
+	  if len(string) <= 1 or string[0] in symbol_dict.values():
+		    return False
+
+	  for symbol in string:
+		    if symbol in symbol_dict.keys():
+			      opening_list.append(symbol_dict[symbol])
+		    else: 
+			      if symbol != opening_list.pop():
+				        return False
+
+    if len(opening_list) == 0:
+        return True
+    else:
+        return False
+
+
+print(balanced_parentheses('[[]]'))
+print(balanced_parentheses('[[]'))
+print(balanced_parentheses('(){}'))
+print(balanced_parentheses(('{{([][])}()}')))
+print(balanced_parentheses('[[]}'))
