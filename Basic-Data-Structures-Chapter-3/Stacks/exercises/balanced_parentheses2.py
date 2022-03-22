62520d89c4456cf426e206940910f5535d840dbc
childmike22 <58493509+childmike22@users.noreply.github.com>
2022-03-21 18:41:34 -0700

    Update balanced_parentheses2.py

--- Basic-Data-Structures-Chapter-3/Stacks/exercises/balanced_parentheses2.py
+++ Basic-Data-Structures-Chapter-3/Stacks/exercises/balanced_parentheses2.py
@@ -8,15 +8,15 @@ symbol_dict = {
 
 def balanced_parentheses(string):
     opening_list = []
-	  if len(string) <= 1 or string[0] in symbol_dict.values():
-		    return False
+    if len(string) <= 1 or string[0] in symbol_dict.values():
+		return False
 
-	  for symbol in string:
-		    if symbol in symbol_dict.keys():
-			      opening_list.append(symbol_dict[symbol])
-		    else: 
-			      if symbol != opening_list.pop():
-				        return False
+    for symbol in string:
+		if symbol in symbol_dict.keys():
+		    opening_list.append(symbol_dict[symbol])
+		else: 
+		    if symbol != opening_list.pop():
+	  	        return False
 
     if len(opening_list) == 0:
         return True
