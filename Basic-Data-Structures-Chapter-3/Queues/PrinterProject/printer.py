0a223f37705cbb7ac01a7b5012a7fcbca478a363
childmike22 <58493509+childmike22@users.noreply.github.com>
2022-03-31 13:20:43 -0700

    Create printer.py

---
+++ Basic-Data-Structures-Chapter-3/Queues/PrinterProject/printer.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+
+class Printer:
+	
+	def __init__(self, ppm):
+	    self.page_rate = ppm
+	    self.current_task = None
+	    self.time_remaining = 0
+
+
+	def tick(self):
+		if self.current_task != None:
+			self.time_remaining = self.time_remaining - 1
+			if self.time_remaining <= 0:
+				self.current_task = None
+
+	def busy(self):
+		return self.current_task != None
+
+
+	def start_next(self, new_task):
+		self.current_task = new_task
+		self.time_remaining = new_task.get_pages() * 60 / self.page_rate
+
